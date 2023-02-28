6a9876cd6690b0e866f355d196fa54cdfc6d80bb
bishal-github <shreesbishal44@gmail.com>
2023-02-28 18:20:10 +0545

    adding hero section to dissplay

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,14 +21,14 @@ const Navbar = () => {
       <div onClick={handleNav} className='md:hidden'>
         {!nav ? <AiOutlineClose  size={30} /> : <AiOutlineMenu size={30} /> }
       </div>
-      <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'fixed left-[-100%]'}>
+      <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'fixed left-[-100%] ease-in-out duration-500'}>
       <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>REACT.</h1>
         <ul className='uppercase p-4'>
           <li className='p-4 border-b border-gray-600'>Home</li>
         <li className='p-4 border-b border-gray-600'>Company</li>
-        <li className='p-4 border-b border-gray-600'>Recources</li>
+        <li className='p-4 border-b border-gray-600'>Resources</li>
         <li className='p-4 border-b border-gray-600'>About</li>
-        <li className='p-4 border-b border-gray-600'>Contact</li> 
+        <li className='p-4'>Contact</li> 
         </ul>
       </div>
     </div>
