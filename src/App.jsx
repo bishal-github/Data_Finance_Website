2516fb7c2f22f85f5103a3b0c77317b2d3afc384
bishal-github <shreesbishal44@gmail.com>
2023-03-01 18:40:39 +0545

    project finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import Analytics from "./components/Analytics"
+import Cards from "./components/Cards"
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
+import Newsletter from "./components/Newsletter"
+import Footer from "./components/Footer"
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Analytics />
+      <Newsletter />
+      <Cards />
+      <Footer />
     </div>
   )
 }
